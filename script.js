5a7b9e0d21fd03a7dc574d6ef11c6f61e721071b
tagiann <115505717+tagiann@users.noreply.github.com>
2023-02-20 14:23:25 +0200

    slider/deleteGrid

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+let input = document.querySelector('[type=range]');
+let dimension = input.value;
+
+let reset = document.querySelector('#reset')
+reset.addEventListener("click", resetGrid)
+
+
+
+createGrid(dimension);
+
+function createGrid(dimension){
+    let grid = document.querySelector('.grid');
+    if (grid === null){
+        let main = document.querySelector('main')
+        grid = document.createElement('div');
+        grid.classList.add("grid");
+        main.appendChild(grid);
     }
-    return color;
+    grid.style["grid-template-columns"] = `repeat(${dimension}, 1fr)`;
+    grid.style["grid-template-rows"] = `repeat(${dimension}, 1fr)`;
+    for (let i = 1; i <= Math.pow(dimension, 2); i++) {
+        let div = document.createElement('div');
+        div.classList.add("default", "grid-item");
+        div.id = `grid-item${i}`;
+        div.addEventListener("mouseout", bringLight)
+        grid.appendChild(div);
+    }
 }
 
 function resetGrid(){
@@ -14,26 +34,32 @@ function resetGrid(){
     })
 }
 
+function deleteGrid(){
+    const grid = document.querySelector('.grid');
+    grid.remove();
+}
+
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+input.addEventListener('change', function(e){
+    dimension = e.target.value;
+    deleteGrid();
+    createGrid(dimension);
+})
+
+
 function bringLight(e) {
     let id = e.fromElement.id;
     let div;
     div = document.querySelector(`[id=${CSS.escape(id)}]`);
     div.style.backgroundColor = getRandomColor();
 }
-
-const grid = document.querySelector('.grid');
-let dimension = 70;
-grid.style["grid-template-columns"] = `repeat(${dimension}, 1fr)`;
-grid.style["grid-template-rows"] = `repeat(${dimension}, 1fr)`;
-for (let i = 1; i <= Math.pow(dimension, 2); i++) {
-    let div = document.createElement('div');
-    div.classList.add("default", "grid-item");
-    div.id = `grid-item${i}`;
-    div.addEventListener("mouseout", bringLight)
-    grid.appendChild(div);
-}
-
-const reset = document.querySelector('#reset')
-reset.addEventListener("click", resetGrid)
-
 
